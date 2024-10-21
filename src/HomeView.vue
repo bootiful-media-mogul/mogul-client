8f09e985a5b6e33e0692142a0f8627fdf812d07b
Josh Long <josh@joshlong.com>
2024-10-21 12:54:51 +0300

    homeview

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -1,46 +1,24 @@
-<script lang="ts">
-import pencilIcon from '@/assets/images/edit.png'
-import pencilIconHighlight from '@/assets/images/edit-highlight.png'
+<script setup lang="ts">
 import InputWrapper from '@/ui/input/InputWrapper.vue'
 import InputTools from '@/ui/InputTools.vue'
+import { ref } from 'vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    InputTools,
-    InputWrapper
-  },
-  methods: {},
-  data() {
-    return {
-      userInput: '',
-      aiIcon: pencilIcon,
-      aiIconHighlight: pencilIconHighlight
-    }
-  }
-}
+const userInput = ref<string>('')
+
 </script>
 
 <template>
   <h1>Home</h1>
-  
-  
   <form class="pure-form pure-form-stacked">
     <fieldset>
       <legend>a nice form</legend>
-
       <div class="pure-control-group">
         <label for="title">input </label>
-
         <InputWrapper v-model="userInput">
-
           <textarea type="text" id="title" rows="10" name="title" :value="userInput"></textarea>
-
           <InputTools v-model="userInput" />
-
         </InputWrapper>
       </div>
-
       <div class="pure-controls">
         <button class="pure-button pure-button-primary" type="submit" value="create">
           do the thing
@@ -48,7 +26,5 @@ export default {
       </div>
     </fieldset>
   </form>
-
   <div>what did the user type? {{ userInput }}</div>
-  
 </template>

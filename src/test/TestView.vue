fff6e6cdebb8eead896872a39dc81e801a536d3b
Josh Long <54473+joshlong@users.noreply.github.com>
2025-07-03 09:51:11 -1000

    up

--- src/test/TestView.vue
+++ src/test/TestView.vue
@@ -1,31 +1,30 @@
 <template>
 
   <div>
-    <a href="#" @click="modal">modal</a> |
-    <a href="#" @click="visible">visible</a> |
-    <a href="#" @click="background">system</a>
+<!--    <a href="#" @click="modal">modal</a> |-->
+<!--    <a href="#" @click="visible">visible</a> |-->
+<!--    <a href="#" @click="background">system</a>-->
   </div>
 
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { notifications } from '@/services'
-
-const visible = async () => {
-  await notifications.notify(true, false)
-}
-
-const modal = async () => {
-  await notifications.notify(false, true)
-}
-
-const background = async () => {
-  await notifications.notify(false, false)
-}
+// import { notifications } from '@/services'
+//
+// const visible = async () => {
+//   await notifications.notify(true, false)
+// }
+//
+// const modal = async () => {
+//   await notifications.notify(false, true)
+// }
+//
+// const background = async () => {
+//   await notifications.notify(false, false)
+// }
 
 onMounted(() => {
-  console.log('test loaded.')
 })
 
 </script>

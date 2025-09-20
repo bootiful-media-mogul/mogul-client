8eeee801effd72dcdc467d20b180bd64d2c73f64
Josh Long <54473+joshlong@users.noreply.github.com>
2025-09-20 13:38:47 +0200

    mock plugin was updated.

--- src/podcasts/publications/Mock.vue
+++ src/podcasts/publications/Mock.vue
@@ -1,17 +1,46 @@
 <script lang="ts" setup>
 import PublicationPanelComponent from '@/publications/PublicationPanelComponent.vue'
 import mockIcon from '@/assets/images/publications/podcasts/mock.png'
-import { inject } from 'vue'
+
+import { inject, onMounted, ref } from 'vue'
+
 import {
   type GetPublicationContextFunction,
+  type IsPluginReadyFunction,
   PublicationContext,
   type PublishFunction
 } from '@/publications/input'
 
+import { notifications } from '@/services'
+
+const isPluginReadyFunction = inject<IsPluginReadyFunction>('isPluginReady')!
+
+const disabled = ref<boolean>(false)
 const publishFunction = inject<PublishFunction>('publish')!
+
+const pluginName = 'mock'
+
 const getPublicationContextFunction =
   inject<GetPublicationContextFunction>('getPublicationContext')!
-const pluginName = 'mock'
+
+async function isPluginDisabled() {
+  const clientContext = {}
+  const publicationContext = getPublicationContextFunction()
+  const ready = await isPluginReadyFunction(
+    publicationContext.type,
+    publicationContext.publishableId,
+    clientContext,
+    pluginName
+  )
+  return !ready!
+}
+onMounted(async () => {
+  disabled.value = await isPluginDisabled()
+})
+
+notifications.listenForCategory('podcast-episode-completed-event', async (evt) => {
+  disabled.value = await isPluginDisabled()
+})
 
 async function publish(): Promise<boolean> {
   const publicationContext: PublicationContext = getPublicationContextFunction()
@@ -29,6 +58,7 @@ async function publish(): Promise<boolean> {
   <PublicationPanelComponent :icon="mockIcon" :icon-hover="mockIcon" plugin="mock">
     <template v-slot:panel>
       <button
+        :disabled="disabled"
         class="pure-button pure-button-primary publish-button"
         type="button"
         @click.prevent="publish()"

33e83f2586d17ca399d5ddfc7eaca84cde0a588b
Josh Long <54473+joshlong@users.noreply.github.com>
2025-11-11 01:25:28 -0800

    search

--- src/search/SearchView.vue
+++ src/search/SearchView.vue
@@ -1,6 +1,12 @@
 <template>
-  search for <em>{{ searchTerm }}</em
-  >.
+  <div>
+    <div>search for {{ searchTerm }}</div>
+    <div>
+      <div v-for="result in results" v-bind:key="result.searchableId">
+        {{ result.searchableId }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,17 +14,21 @@ import { events, search } from '@/services'
 import { onMounted, ref } from 'vue'
 
 const searchTerm = ref<string>('')
+const results = ref([] as Array<Map<string, any>>)
 
 async function doSearch(q: string) {
-  const results = await search.search(q)
-  console.log(results)
+  searchTerm.value = q
+  const r: Array<any> = []
+  const apiResults = await search.search(q)
+  apiResults.forEach((v) => {
+    r.push(v)
+  })
+  results.value = r
 }
 
 onMounted(async () => {
   events.on('search-term-entered', async (event) => {
-    console.log(event)
-    searchTerm.value = event as string
-    await doSearch(searchTerm.value)
+    await doSearch(event as string)
   })
 })
 </script>

552ef0d710ba30ba55920e58b3129c36ee4d0d8a
Josh Long <joshua.long@broadcom.com>
2026-09-13 15:15:48 -0500

    adding ayrshare plugin for moguls

---
+++ src/home/publications/Ayrshare.vue
@@ -0,0 +1,7 @@
+<template>
+  <AyrshareComponent :icon="ayrshareIcon" plugin-name="mogulAyrshare" />
+</template>
+<script lang="ts" setup>
+import ayrshareIcon from '@/assets/images/publications/mogul/publish-to-ayrshare.png'
+import AyrshareComponent from '@/publications/AyrshareComponent.vue'
+</script>
